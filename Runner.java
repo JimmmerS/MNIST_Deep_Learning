f0a91bd5ab9bc022b7c39aea761db6be70b21e1b
Jim <jws6720@rit.edu>
2018-05-17 22:11:19 -0400

    Added a simple javafx framework to display the inputs and outputs

--- Runner.java
+++ Runner.java
@@ -1,6 +1,36 @@
-public class Runner {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Runner extends Application{
+
+    @Override
+    public void start(Stage primaryStage){
+
+        Button btn = new Button("Start");
+
+        btn.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event){
+                System.out.println("Lots of deep learning going on...");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("MNIST Deep Learning");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args){
-
+        launch(args);
     }
 }
