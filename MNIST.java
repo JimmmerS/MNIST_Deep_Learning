b37f90c311fe2fdfa571aa711095379b12543759
Jim <jws6720@rit.edu>
2018-05-17 23:15:15 -0400

    Some GZip file reading has been added

--- MNIST.java
+++ MNIST.java
@@ -2,25 +2,52 @@
     This class will connect to the MNIST database and pull in new images
  */
 
-import org.python.modules.cPickle;
+
+import java.io.*;
+import java.util.zip.GZIPInputStream;
 
 public class MNIST {
 
-    private static final int TRAINING_SIZE = 50000;
+    // These correspond to the subset of MNIST data in the .gz files
+    private static final int TRAINING_SIZE = 60000;
     private static final int VALIDATION_SIZE = 10000;
     private static final int TEST_SIZE = 10000;
 
-    public MNIST (){
+    private static final String train_images_file = "train-images-ubyte.gz";
+    private static final String train_labels_file = "train-labels-ubyte.gz";
+    private static final String test_images_file = "test-images-ubyte.gz";
+    private static final String test_labels_file = "test-labels-ubyte.gz";
 
+    private NumberImage[] trainingImages;
+    private NumberImage[] testImages;
+
+    public MNIST (){
+        trainingImages = new NumberImage[TRAINING_SIZE];
+        testImages = new NumberImage[TEST_SIZE];
+
+
+        try {
+            DataInputStream trainImageStream = new DataInputStream(new GZIPInputStream(new FileInputStream(
+                    train_images_file)));
+            DataInputStream trainLabelStream = new DataInputStream(new GZIPInputStream(new FileInputStream(
+                    train_labels_file)));
+            DataInputStream testImageStream = new DataInputStream(new GZIPInputStream(new FileInputStream(
+                    test_images_file)));
+            DataInputStream testLabelStream = new DataInputStream(new GZIPInputStream(new FileInputStream(
+                    test_labels_file)));
+        } catch (IOException e){
+            System.out.println("Data file couldn't be read");
+            System.exit(1);
+        }
     }
 
-    class image {
+    class NumberImage {
         private int value;
 
         // Each value is between 0x00 and 0xFF
         private byte[][] data;
 
-        public image (){
+        public NumberImage (){
             data = new byte[255][255];
             value = 0;
 
